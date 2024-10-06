32d48f700412e24a824e5f9e6af7cdd3b086b36c
zeriong <jaeryong95@gmail.com>
2024-10-06 16:09:04 +0900

    기본적인 dataType과 num이라는 타입에 대한 학습

---
+++ dataType.dart
@@ -0,0 +1,15 @@
+void main() {
+  String name = "이름";
+  bool boolean = false;
+  int n = 1;
+  double D = 1.1;
+
+  // 객체 지향 언어로 int, double등을 들어가 보면 모두 num이란 오브젝트를 부모로 가지고 있음
+  // 즉 num 타입은 숫자에 대한 모든 타입을 지정할 수 있다는 것
+  num number = 1;
+  number = 1.23;
+  number = 1.2;
+  number = 0;
+
+
+}
