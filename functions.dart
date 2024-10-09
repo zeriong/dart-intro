d5caf2874f81197af0e5ea1f55e5c0f91355effc
jaery <jaeryong95@gmail.com>
2024-10-09 19:22:20 +0900

    named argument를 활용함 함수 사용법 학습

--- functions.dart
+++ functions.dart
@@ -1,4 +1,3 @@
-
 // void란 의미적으로 return문을 활용하여 반환하는 값이 없다는 뜻
 void sayHello(String name) {
   print("Hello $name nice to meet you!");
@@ -15,6 +14,17 @@ String sayHelloArrowFunction(String name) => "Hello $name?";
 // 화살표 함수를 활용한 좋은 예시(코드 최적화가 가능해진 예)
 num plus(num a, num b) => a + b;
 
+// named argument 를 활용한 예시
+// default value를 지정하지 않는다면 optional chaining을 통해 null이 허용되게 만들어야 함.
+// 하지만 이는 Hello my name is null, 이런 형태로 적용되기 때문에 적합하지 않음
+String introduceUser({
+  String name = "Minsu",
+  int age = 20,
+  String country = "GongSaPhan",
+}) {
+  return "Hello my name is $name, I'm $age years old and from $country";
+}
+
 /**  */
 void main() {
   sayHello("zeriong");
@@ -27,4 +37,7 @@ void main() {
 
   // plus 함수 프린트
   print("plus result: ${plus(5, 7)}");
+
+  // named argument를 활용한 예시 프린트
+  print(introduceUser(age: 29, name: "zeriong", country: "Korea"));
 }
