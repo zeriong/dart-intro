a4613f52ea88020569ef0d67e3bd7cb8a9421fb3
zeriong <jaeryong95@gmail.com>
2024-10-13 18:04:54 +0900

    클래스가 가진 메서드 사용하는 방식 학습

--- classes.dart
+++ classes.dart
@@ -1,7 +1,16 @@
 // 클래스 정의 방식은 JAVA와 유사하다
 class Player {
+  // final로 상수 선언
+  final immerName = "can't modify zeriong";
   String name = "zeriong";
   int xp = 1500;
+
+  void sayHello() {
+    print("Hello my name is \"${immerName}\"");
+    // this 활용도 가능하지만 Dart에서는 권장하지 않는 방식이다.
+    // 하지만 가독성과 복잡성을 고려한다면 활용해도 문제되지 않을 것 같다.
+    // print("Hello my name is ${this.immerName}");
+  }
 }
 
 void main() {
@@ -11,4 +20,7 @@ void main() {
   // 기존의 JS, JAVA에서 활용하던 방식대로 사용 가능하다
   player.name = "new zeriong";
   print(player.name);
+
+  // immerName을 출력하는 매서드
+  player.sayHello();
 }
