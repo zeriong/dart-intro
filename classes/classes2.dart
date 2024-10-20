ec86acc35dcb97e02fd2e5553173373d8cd1f4f8
zeriong <jaeryong95@gmail.com>
2024-10-20 19:05:36 +0900

    abstract class(추상화 클래스)에 대한 학습

--- classes/classes2.dart
+++ classes/classes2.dart
@@ -3,7 +3,15 @@
 // 데이터를 넣는 방식으로 이해 가능함.
 enum Team { red, blue }
 
-class Player {
+// 추상화 클래스 정의
+abstract class Human {
+  void walk();
+}
+
+// 추상화 클래스를 상속하여 반드시 walk매서드를 가지도록 강제할 수 있음.
+// 이는 전체적인 추상화에 도움이 될 수 있음
+// ( walk 매서드가 없다면 에러 발생 )
+class Player extends Human {
   final String name;
   int age, xp; // 동시 정의
   Team team; // 이넘을 적용
@@ -16,14 +24,29 @@ class Player {
     required this.team,
   });
 
+  // 추상화 클래스 Human이 필요로하는 method를 추가해줌
+  void walk() {
+    print("$name is walking");
+  }
+
   // player info print method
   void status() {
     print("name: \"$name\", xp: $xp, team: \"$team\", age: $age");
   }
 }
 
+class Coach extends Human {
+  void walk() {
+    print("coach is walking!");
+  }
+}
+
 /** main */
 void main() {
-  var player1 = Player(name: "name", xp: 10, team: Team.red, age: 30);
+  var player1 = Player(name: "zeriong", xp: 10, team: Team.red, age: 30);
   player1.status();
+  player1.walk();
+
+  var coach = Coach();
+  coach.walk();
 }
